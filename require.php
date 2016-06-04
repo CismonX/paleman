e63b55173d055cedd9b6cca2466e61b282560b70
乔成 <qiaocheng@twt.edu.cn>
2016-06-04 16:24:46 +0800

    Update require.php

--- require.php
+++ require.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/Workerman/Lib/Channel/Client.php';
 require_once __DIR__ . '/Workerman/Lib/GlobalData/Client.php';
 
-foreach(glob(__DIR__ . '/../Application/*.php') as $require_file){
+//Path for files created by user.
+foreach(glob(__DIR__ . '/../Application/*.php') as $require_file) {
     require_once $require_file;
 }
