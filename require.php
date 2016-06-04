3f28c69e76bf6cdcad3ca862bc270aaf905214a9
乔成 <qiaocheng@twt.edu.cn>
2016-06-04 12:53:14 +0800

    Add new file

---
+++ require.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/Workerman/Lib/Channel/Client.php';
+require_once __DIR__ . '/Workerman/Lib/GlobalData/Client.php';
+
+foreach(glob(__DIR__ . '/../Application/*.php') as $require_file){
+    require_once $require_file;
+}
