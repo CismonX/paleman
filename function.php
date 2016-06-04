54046a3f4847cc7da5d71391fc128d75aa32cf30
乔成 <qiaocheng@twt.edu.cn>
2016-06-04 16:23:03 +0800

    Update function.php

--- function.php
+++ function.php
@@ -3,16 +3,20 @@
 require_once __DIR__ . '/require.php';
 
 function parse_request($request_str) {
+    //Use spaces to indent strings.
     $request_arr = explode(' ', $request_str);
-    //add, del
     $operation = $request_arr[0];
+    //Operation types.
     switch($operation) {
+        //Format: <operation_type> <interval> <application_type> <argument_1> <argument_2> ...
         case 'add':
+            //Call user-defined parser function.
             $request = call_user_func($request_arr[2].'_parse', array_slice($request_arr, 3));
             if($request === false)
                 return false;
             $request['interval'] = (int)$request_arr[1];
             break;
+        //Format: <operation_type> <listener_id>
         case 'del':
             $request['listener_id'] = $request_arr[1];
             break;
@@ -21,13 +25,4 @@ function parse_request($request_str) {
     }
     $request['operation'] = $operation;
     return $request;
-}
-
-function timer_callback(callable $timer_func, $args, $data = null) {
-    $send_data = call_user_func_array($timer_func, array_values($args));
-    if($send_data === false)
-        return;
-    for($worker = 0; $worker < 4; $worker++){
-        Channel\Client::publish('send_'.$worker, $send_data);
-    }
 }
