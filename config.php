4955be6f95c4e8879a5a587d85da93349efef081
乔成 <qiaocheng@twt.edu.cn>
2016-06-09 22:05:03 +0800

    Add new file

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+//Constants.
+define('AUTOLOADER_ROOT_PATH', __DIR__);
+define('WORKERMAN_PATH', __DIR__ . '/Workerman');
+define('APPLICATION_PATH', __DIR__ . '/../Application');
+define('WEB_PATH', APPLICATION_PATH . '/web');
+define('CONTROL_WORKER_COUNT', 4);
+define('TASK_WORKER_COUNT', 8);
+define('SEND_WORKER_COUNT', 4);
+define('CHANNEL_ADDR', '127.0.0.1');
+define('GLOBALDATA_ADDR', '127.0.0.1');
+define('WEB_PORT', 80);
+define('SEND_PORT', 2000);
+define('CONTROL_PORT', 2001);
+define('CHANNEL_PORT', 2206);
+define('GLOBALDATA_PORT', 2207);
+define('CORS_DOMAIN', '*');
+define('DEFAULT_INTERVAL', 60);
+define('DEFAULT_ARGS_ADD', array());
+define('DEFAULT_RETURN_ADD', array('task_id', 'interval', 'task_name'));
+define('DEFAULT_ARGS_CFG', array());
+define('DEFAULT_RETURN_CFG', array('task_name'));
+
+require_once WORKERMAN_PATH . '/Lib/Channel/Client.php';
+require_once WORKERMAN_PATH . '/Lib/GlobalData/Client.php';
+
+//Require files created by user.
+foreach (glob(APPLICATION_PATH.'/*.php') as $require_file) {
+    require_once $require_file;
+}
