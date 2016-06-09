193e227a3bf8fe1f51904031e946e29a654af019
乔成 <qiaocheng@twt.edu.cn>
2016-06-09 22:01:58 +0800

    Delete require.php

--- require.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/Workerman/Lib/Channel/Client.php';
-require_once __DIR__ . '/Workerman/Lib/GlobalData/Client.php';
-
-//Path for files created by user.
-foreach(glob(__DIR__ . '/../Application/*.php') as $require_file) {
-    require_once $require_file;
-}
