420cc398a15306228f0d616a2137ec158473c611
乔成 <qiaocheng@twt.edu.cn>
2016-06-09 22:03:16 +0800

    Update function.php

--- function.php
+++ function.php
@@ -1,28 +1,113 @@
 <?php
 
-require_once __DIR__ . '/require.php';
-
 function parse_request($request_str) {
     //Use spaces to indent strings.
     $request_arr = explode(' ', $request_str);
     $operation = $request_arr[0];
-    //Operation types.
-    switch($operation) {
-        //Format: <operation_type> <interval> <application_type> <argument_1> <argument_2> ...
+    //Operation type.
+    switch ($operation) {
+        //Format: add <task_name> <argument_1> <argument_2> ...
         case 'add':
-            //Call user-defined parser function.
-            $request = call_user_func($request_arr[2].'_parse', array_slice($request_arr, 3));
-            if($request === false)
-                return false;
-            $request['interval'] = (int)$request_arr[1];
+            if (count($request_arr) < 3){
+                $operation = 'err';
+                $request['err'] = 'Invalid arguments.';
+                break;
+            }
+            //Call user-defined function to parse add request.
+            $task_name = $request_arr[1];
+            if (!is_callable($task_name.'_add')){
+                $operation = 'err';
+                $request['err'] = 'Add function not callable.';
+                break;
+            }
+            $request = call_user_func($task_name.'_add', array_slice($request_arr, 2));
+            if (isset($request['err'])) {
+                $operation = 'err';
+                break;
+            }
+            $request['task_name'] = $task_name;
             break;
-        //Format: <operation_type> <task_id>
+        //Format: cfg <configure_type> <argument_1> <argument_2> ...
+        case 'cfg':
+            if (count($request_arr) < 2) {
+                $operation = 'err';
+                $request['err'] = 'Invalid arguments.';
+                break;
+            }
+            $func = $request_arr[1] . '_cfg';
+            if (!is_callable($func)) {
+                $operation = 'err';
+                $request['err'] = 'Configure function not callable.';
+                break;
+            }
+            //Call user-defined function to parse configure request.
+            $request = call_user_func($func, array_slice($request_arr, 2));
+            $request['task_name'] = $request_arr[1];
+            break;
+        //Format: del <task_id>
         case 'del':
+            if (count($request_arr) < 2) {
+                $operation = 'err';
+                $request['err'] = 'Invalid arguments.';
+                break;
+            }
+            $data = getGlobalData($request_arr[1], array('timer_id', 'worker_id', 'task_name'));
+            if ($data === false) {
+                $operation = 'err';
+                $request_arr[1] = addslashes($request_arr[1]);
+                $request['err'] = "Task ID $request_arr[1] do not exist.";
+                break;
+            }
             $request['task_id'] = $request_arr[1];
+            $request['timer_id'] = $data['timer_id'];
+            $request['worker_id'] = $data['worker_id'];
+            $request['task_name'] = $data['task_name'];
             break;
         default:
-            return false;
+            $operation = 'err';
+            $request['err'] = 'Invalid operation type.';
+            break;
     }
     $request['operation'] = $operation;
     return $request;
 }
+
+function getGlobalData($task_id, $key) {
+    global $global;
+    if(!isset($global->$task_id))
+        return false;
+    if(!isset($global->$task_id[$key]))
+        return null;
+    $data = $global->$task_id[$key];
+    return $data;
+}
+
+function getGlobalData_array($task_id, array $keys) {
+    global $global;
+    if(!isset($global->$task_id))
+        return false;
+    $data = array();
+    foreach ($keys as $key) {
+        $data[$key] = $global->$task_id[$key];
+    }
+    return $data;
+}
+//Set or unset GlobalData. (atomic operation)
+function setGlobalData($task_id, $key = null, $data = null) {
+    global $global;
+    if (!isset($global->$task_id))
+        return false;
+    if ($key === null) {
+        unset($global->$task_id);
+        return true;
+    }
+    do {
+        $old_data = $new_data = $global->$task_id;
+        if ($data === null)
+            unset($new_data[$key]);
+        else
+            $new_data[$key] = $data;
+    } while(!$global->cas($task_id, $old_data, $new_data));
+    return true;
+}
+
