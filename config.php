e5f50fcc85ec7d34e35a7aad56a4a13159ee95ba
CismonX <qiaocheng@twt.edu.cn>
2016-07-01 14:45:20 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
 
 //Constants.
-define('AUTOLOADER_ROOT_PATH', __DIR__);
-define('WORKERMAN_PATH', __DIR__ . '/Workerman');
+define('WORKERMAN_PATH', __DIR__ . '/../Workerman');
 define('APPLICATION_PATH', __DIR__ . '/../Application');
 define('WEB_PATH', APPLICATION_PATH . '/web');
 define('CONTROL_WORKER_COUNT', 4);
@@ -19,9 +18,12 @@ define('CORS_DOMAIN', '*');
 define('DEFAULT_INTERVAL', 60);
 define('DEFAULT_ARGS_ADD', array());
 define('DEFAULT_RETURN_ADD', array('task_id', 'interval', 'task_name'));
-define('DEFAULT_ARGS_CFG', array());
-define('DEFAULT_RETURN_CFG', array('task_name'));
+define('DEFAULT_ARGS_SET', array());
+define('DEFAULT_RETURN_SET', array('task_name'));
 
+//Require files.
+require_once WORKERMAN_PATH . '/Lib/Channel/Server.php';
+require_once WORKERMAN_PATH . '/Lib/GlobalData/Server.php';
 require_once WORKERMAN_PATH . '/Lib/Channel/Client.php';
 require_once WORKERMAN_PATH . '/Lib/GlobalData/Client.php';
 
