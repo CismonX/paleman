a3f29a7a60a65263320ac7af2da25f8f0dcff0dc
CismonX <qiaocheng@twt.edu.cn>
2016-07-12 16:30:51 +0800

    Update config.php

--- Paleman/config.php
+++ Paleman/config.php
@@ -16,10 +16,10 @@ define('CHANNEL_PORT', 2206);
 define('GLOBALDATA_PORT', 2207);
 define('CORS_DOMAIN', '*');
 define('DEFAULT_INTERVAL', 60);
-define('DEFAULT_ARGS_ADD', array());
-define('DEFAULT_RETURN_ADD', array('task_id', 'interval', 'task_name'));
-define('DEFAULT_ARGS_SET', array());
-define('DEFAULT_RETURN_SET', array('task_name'));
+define('DEFAULT_ARGS_ADD', []);
+define('DEFAULT_RETURN_ADD', ['task_id', 'interval', 'task_name']);
+define('DEFAULT_ARGS_SET', []);
+define('DEFAULT_RETURN_SET', ['task_name']);
 
 //Require Paleman library.
 foreach (glob(__DIR__ . '/Lib/*.php') as $require_file) {
